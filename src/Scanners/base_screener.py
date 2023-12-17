69ab3962e465a4f93a7062927aa89a86c1b0a5ae
Mohit Raj Rathor <mohitrajrathor@gmail.com>
2023-12-17 19:09:30 +0530

    adds some code

--- src/Scanners/base_screener.py
+++ src/Scanners/base_screener.py
@@ -1,5 +1,17 @@
+# Auther - Mohit Raj Rathor
 # python file to create and store screeners from base screener class.
 
+import pandas as pd
 
 class Scanner:
-    pass
+    def __init__(self, data:pd.DataFrame) -> None:
+        """
+        base screener class
+            params : 
+                data -> pandas datframe consist ["time", "open", "high", "low", "close", "volume"]
+
+        """
+        self.data = data
+
+    def scan_result(self) -> pd.DataFrame:
+        pass 
--- src/Strategies/base_strategy.py
+++ src/Strategies/base_strategy.py
@@ -1,4 +1,7 @@
+# Auther - Mohit Raj Rathor.
 # base strategy class for creating strategies.
+
+
 
 class Strategy:
     pass
