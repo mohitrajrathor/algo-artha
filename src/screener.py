a93e3da9966b418e8e6894d0906c1053246031cb
Mohit Raj Rathor <mohitrajrathor@gmail.com>
2024-01-01 23:46:37 +0530

    top gainer & looser screener created

--- src/screener.py
+++ src/screener.py
@@ -1,9 +1,22 @@
+# -*- coding : utf-8 -*-
+"""
+Created on - Mon Jan 01 2024 23:37 IST
+@author : Mohit Raj Rathor
+"""
+
 # screener class
 
+from pandas.core.api import DataFrame as DataFrame
 import yfinance as yf
 import pandas as pd 
 import pandas_ta
 import datetime as dt 
+from pprint import pprint
+
+
+############### utils functions ###############
+
+
 
 
 class Screener:
@@ -14,13 +27,20 @@ class Screener:
         """
         self.symbols = symbols
         if info is None:
-            self.info = "Base screener class"
+            self.info = "Screen stocks from given symbols that are above 20 moving average."
         else:
             self.info = info
 
-    def get_data(self, symbol:str, days:int = 300) -> pd.DataFrame:
+        def __str__(self) -> str:
+            return f"Screener class\n{self.info}"
+
+    def get_data(self, symbol:str, days:int=300, exchange=".NS") -> pd.DataFrame:
         """
         fatch data from yahoo finance using yfinance.
+        param:
+            symbol -> symbol of stocks for screening
+            days -> no. days data you want to filter
+            exchange -> '.NS' for NSE or '.BO' for "BSE".
         return -> pd.DataFrame (
                 index -> 'date'
                 columns -> ['open', 'high', 'low', 'close', 'adj close', 'volume']
@@ -61,20 +81,66 @@ class Screener:
             if self.condition(self.get_data(i)):
                 screened_sym.append(i)
             
+        screened_sym = [i.split('.')[0] for i in screened_sym]      # removing .NS from end
         return screened_sym
     
 
 class Top_gainer_looser_screener(Screener):
     def __init__(self, symbols: list[str], info: str = None) -> None:
         super().__init__(symbols, info)
+        self.info = "Screen previous day's top gainer and looser stocks of given list "       # over-write self.info of screenr class
 
 
-class tp:
-    def __init__(self) -> None:
-        print(self)
+    def get_data(self, symbol:list[str], exchange=".NS") -> DataFrame:
+        """
+        fatch data from yahoo finance using yfinance.
+        param:
+            symbol -> symbol of stocks for screening
+            exchange -> '.NS' for NSE or '.BO' for "BSE".
+        return -> pd.DataFrame (
+                index -> 'date'
+                columns -> ['open', 'high', 'low', 'close', 'adj close', 'volume']
+        )
+            NOTE : it will return data of all stocks simultenously
+        """
+        symbols = [sym+exchange for sym in self.symbols]
+        start_date = (dt.datetime.now() - dt.timedelta(days=1)).strftime("%Y-%m-%d") 
+        data = yf.download(tickers=symbols, start=start_date) 
+        data = data.stack()      
+        data.index.names = ['Date', 'Symbol']
+        data = data.reset_index()
+        return data
+    
+    def condition(self, data: DataFrame) -> dict:
+        # let's get insights from data
+        data['change'] = ((data['Adj Close'] - data['Open']) / data['Open'] * 100).round(2)
+
+        # get the gainers
+        gainers = data.sort_values(by=["change"], ascending=False).iloc[:10]['Symbol'].to_list()
+        gainers = [i.split('.')[0] for i in gainers]
+
+        # get the loosers
+        loosers = data.sort_values(by=["change"], ascending=True).iloc[:10]['Symbol'].to_list()
+        loosers = [i.split('.')[0] for i in loosers]
+
+        print()
+
+        result = {
+            'gainers' : gainers,
+            'looser' : loosers
+        }
+
+        return result
+
+    def screen(self) -> dict:
+        return self.condition(self.get_data(self.symbols))
 
 
     
 # testing 
 if __name__ == "__main__":
-    any = tp()
+    nifty50 = pd.read_csv('./data/nifty_50.csv')['Symbol'].to_list()
+
+    gl = Top_gainer_looser_screener(nifty50)
+    result = gl.screen()
+    pprint(result)
